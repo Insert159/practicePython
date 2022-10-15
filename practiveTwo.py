8d07c59fd5334fbe9751d54bd430674278d51ec4
Алексей <insert159@mail.ru>
2022-10-15 10:46:33 +0600

    first HW is done

---
+++ practiveTwo.py
@@ -0,0 +1,11 @@
+# Напишите программу, которая принимает на вход число N 
+# и выдаёт последовательность из N членов.
+
+
+
+# from random import randint
+
+# n = int(input("введите число "))
+
+# for numbers in range(n):
+#     print (randint(-100, 100), end=" ")
